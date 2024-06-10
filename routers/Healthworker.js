df1a30f4ee1b3d62a7d886058cb8dca7e1486ab0
EMMANUEL <83466358+Emmanuel19-code@users.noreply.github.com>
2024-06-10 10:59:15 +0000

    update

--- routers/Healthworker.js
+++ routers/Healthworker.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { registerHealthworkeraccount } from "../HealthworkerController/registerhealthworkers.js";
+import { getAllHealthworkers } from "../HealthworkerController/getHealthworkers.js";
 
 const router = express.Router();
 
 router.post("/registerhealthworker", registerHealthworkeraccount);
+router.get("/healthworker",getAllHealthworkers)
