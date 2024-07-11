639bfe9a04281b1ae68e25e246d48df7542db1fc
EMMANUEL <83466358+Emmanuel19-code@users.noreply.github.com>
2024-07-11 22:10:51 +0000

    update

--- Conversationcontroller/InvoledConversation.js
+++ Conversationcontroller/InvoledConversation.js
@@ -5,20 +5,7 @@ import client from "../database/redis.js";
 export const InvolvedConversation = async (req, res) => {
   try {
     const userId = req.user.uniqueId;
-    const cacheKey = `conversations:${userId}`;
-    // Ensure Redis client is connected
-    if (!client.isOpen) {
-      await client.connect();
-    }
-    // Check if the data is in the cache
-    const cachedData = await client.get(cacheKey);
-    if (cachedData) {
-      // If data is found in the cache, return it
-      return res.status(200).json({
-        data: JSON.parse(cachedData),
-      });
-    } else {
-      // If data is not in the cache, fetch it from the database
+   
       const conversations = await conversation.find({ participants: userId });
       if (!conversations || conversations.length === 0) {
         return res.status(404).json({
@@ -28,7 +15,7 @@ export const InvolvedConversation = async (req, res) => {
       const conversationUserData = await Promise.all(
         conversations.map(async (conversation) => {
           const notUser = conversation.participants.find((id) => id != userId);
-          const consultant = await health_worker.find({
+          const consultant = await health_worker.findOne({
             healthworkerId: notUser,
           });
 
@@ -43,11 +30,11 @@ export const InvolvedConversation = async (req, res) => {
           };
         })
       );
-      await client.setEx(cacheKey, 1800, JSON.stringify(conversationUserData)); //holding data for a day
+     
       return res.status(200).json({
         data: conversationUserData,
       });
-    }
+    
   } catch (err) {
     console.error("Error fetching user conversations:", err);
 
