a5c891ad3fee923246c27a43094cb9f5ff455449
EMMANUEL <83466358+Emmanuel19-code@users.noreply.github.com>
2024-07-30 12:45:15 +0000

    update

--- routers/notifications.js
+++ routers/notifications.js
@@ -2,11 +2,15 @@ import express from "express"
 import { SendNotifcationToUser } from "../NotificationController/sendToUser.js"
 import {
   HealthworkerAuthetication,
+  Authentication
 } from "../middlewares/authentication.js";
 import { SendNotifcationToAll } from "../NotificationController/sendToAll.js";
+import { UserGetNotification } from "../NotificationController/UserGetNotice.js";
+
 const router = express.Router()
 
 router.post("/send_to_user",HealthworkerAuthetication,SendNotifcationToUser)
 router.post("/send_to_all",HealthworkerAuthetication,SendNotifcationToAll)
+router.get("/all_notifications",Authentication,UserGetNotification)
 
 export default router
